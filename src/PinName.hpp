032bb06ec270852e71918ed563aca1816524290b
git-Shoyo <mc.merry57@gmail.com>
2025-07-01 15:28:09 +0900

    change: update Libraries and pin map

--- src/PinName.hpp
+++ src/PinName.hpp
@@ -10,43 +10,43 @@
 
 // mecanum wheel pwm pin name
 // set 0 to 15 or GPIO pin name
-#define WHEEL_FR_PWM _0
-#define WHEEL_FL_PWM _1
-#define WHEEL_BR_PWM _2
-#define WHEEL_BL_PWM _3
+#define WHEEL_FR_PWM 0
+#define WHEEL_FL_PWM 1
+#define WHEEL_BR_PWM 2
+#define WHEEL_BL_PWM 3
 
 // mecanum wheel dir pin name
 // set 0 to 15 or GPIO
-#define WHEEL_FR_DIR _GPIO21
-#define WHEEL_FL_DIR _GPIO20
-#define WHEEL_BR_DIR _GPIO16
-#define WHEEL_BL_DIR _GPIO12
+#define WHEEL_FR_DIR 21
+#define WHEEL_FL_DIR 20
+#define WHEEL_BR_DIR 16
+#define WHEEL_BL_DIR 12
 
 // left arm motor pin name
 // set 0 to 15 or GPIO
-#define LEFT_SHOULDER_PWM _5
-#define LEFT_ELBOW_PWM _6
+#define LEFT_SHOULDER_PWM 5
+#define LEFT_ELBOW_PWM 6
 
-#define LEFT_SHOULDER_DIR _12
-#define LEFT_ELBOW_DIR _13
+#define LEFT_SHOULDER_DIR 12
+#define LEFT_ELBOW_DIR 13
 
 // right arm motor pin name
 // set 0 to 15 or GPIO
-#define RIGHT_SHOULDER_PWM _7
-#define RIGHT_ELBOW_PWM _8
+#define RIGHT_SHOULDER_PWM 7
+#define RIGHT_ELBOW_PWM 8
 
-#define RIGHT_SHOULDER_DIR _14
-#define RIGHT_ELBOW_DIR _15
+#define RIGHT_SHOULDER_DIR 14
+#define RIGHT_ELBOW_DIR 15
 
 // slider motor pin name
 // set 0 to 15 or GPIO
-#define FRONT_SLIDER_PWM _10
-#define BACK_SLIDER_PWM _11
+#define FRONT_SLIDER_PWM 10
+#define BACK_SLIDER_PWM 11
 
-#define FRONT_SLIDER_DIR _9
-#define BACK_SLIDER_DIR _8
+#define FRONT_SLIDER_DIR 9
+#define BACK_SLIDER_DIR 8
 
 // air motor pin name
 //set 0 to 15 or GPIO
-#define LEFT_ARM_AIR_PWM _EMPTY
-#define RIGHT_ARM_AIR_PWM _EMPTY
+#define LEFT_ARM_AIR_PWM EMPTY
+#define RIGHT_ARM_AIR_PWM EMPTY
